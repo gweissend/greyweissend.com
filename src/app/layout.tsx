c853032a29b329ea0af543e90b49da4bd699fd0f
gweissend <greyweissend@gmail.com>
2023-09-16 16:21:43 -0400

    commenting out potential issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,15 +9,15 @@ export const metadata: Metadata = {
   title: {
     template: '%s - Grey Weissend',
     default:
-      'Grey Weissend-*-- - Software designer, founder, and amateur astronaut',
+      'Grey Weissend-*-- - Explorer, designer and creator.',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
-  alternates: {
-    types: {
-      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
-    },
-  },
+    'Hey there',
+  // alternates: {
+  //   types: {
+  //     'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+  //   },
+  // },
 }
 
 export default function RootLayout({
