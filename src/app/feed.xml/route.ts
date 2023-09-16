5ddb43d22ea6afc569091b4e07596fd483693f25
gweissend <greyweissend@gmail.com>
2023-09-16 13:56:07 -0400

    changed env variable, made site URL a string

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -3,15 +3,15 @@ import * as cheerio from 'cheerio'
 import { Feed } from 'feed'
 
 export async function GET(req: Request) {
-  let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
+  let siteUrl = process.env._NEXT_PUBLIC_SITE_URL
 
   if (!siteUrl) {
     throw Error('Missing NEXT_PUBLIC_SITE_URL environment variable')
   }
 
   let author = {
-    name: 'Spencer Sharp',
-    email: 'spencer@planetaria.tech',
+    name: 'Grey Weissend',
+    email: 'greyweissend@gmail.com',
   }
 
   let feed = new Feed({
