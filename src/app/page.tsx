a177bb55438143e7446603cca88a3ffae79e76b8
gweissend <greyweissend@gmail.com>
2023-09-16 17:17:29 -0400

    working through edge issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image, { type ImageProps } from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
-export const runtime = 'edge' // 'nodejs' (default) | 'edge'
 
 import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
