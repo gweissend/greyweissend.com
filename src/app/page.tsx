6fdd58f1bba27b857e02d736ec51e524a6b957a2
gweissend <why.grey.matter@gmail.com>
2023-09-19 14:57:30 -0400

    updated resume link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -242,7 +242,7 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href='https://drive.google.com/file/d/1YYY1Ir-70GjBT2vasfnQyYOBVCupFZTZ/view?usp=sharing' variant="secondary" className="group mt-6 w-full">
+      <Button href='https://drive.google.com/file/d/1j7q2wEsiD9twBxifQcDNZFaCuFQ7xQFr/view?usp=sharing' variant="secondary" className="group mt-6 w-full">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
